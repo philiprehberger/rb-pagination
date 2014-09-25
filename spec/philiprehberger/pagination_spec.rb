49738d51f517a372aa8fa57366c78bb73972c797
Reverend Philip <me@philiprehberger.com>
2014-09-24 18:05:07 -0600

    Release v0.1.1

--- spec/philiprehberger/pagination_spec.rb
+++ spec/philiprehberger/pagination_spec.rb
@@ -118,6 +118,80 @@ RSpec.describe Philiprehberger::Pagination do
     end
   end
 
+  describe 'page edge cases' do
+    it 'handles negative page number as page 1' do
+      page = described_class.paginate(items, strategy: :offset, per_page: 10, page: -5)
+      expect(page.items).to eq((1..10).to_a)
+    end
+  end
+
+  describe 'total pages calculation' do
+    it 'calculates total pages for exact division' do
+      page = described_class.paginate(items, strategy: :offset, per_page: 10)
+      total_pages = (page.total.to_f / 10).ceil
+      expect(total_pages).to eq(5)
+    end
+
+    it 'calculates total pages for non-exact division' do
+      page = described_class.paginate(items, strategy: :offset, per_page: 7)
+      total_pages = (page.total.to_f / 7).ceil
+      expect(total_pages).to eq(8)
+    end
+  end
+
+  describe 'offset calculation' do
+    it 'returns correct offset items for page 2 with per_page 5' do
+      page = described_class.paginate(items, strategy: :offset, per_page: 5, page: 2)
+      expect(page.items).to eq([6, 7, 8, 9, 10])
+    end
+
+    it 'returns correct offset items for last partial page' do
+      small = (1..13).to_a
+      page = described_class.paginate(small, strategy: :offset, per_page: 5, page: 3)
+      expect(page.items).to eq([11, 12, 13])
+    end
+  end
+
+  describe 'per_page edge cases' do
+    it 'treats per_page of 1 as single-item pages' do
+      page = described_class.paginate(items, strategy: :offset, per_page: 1, page: 1)
+      expect(page.items).to eq([1])
+      expect(page.has_next?).to be true
+    end
+
+    it 'treats very large per_page as single page' do
+      page = described_class.paginate(items, strategy: :offset, per_page: 1000)
+      expect(page.items).to eq(items)
+      expect(page.has_next?).to be false
+    end
+  end
+
+  describe 'empty collection' do
+    it 'returns empty page for empty collection' do
+      page = described_class.paginate([], strategy: :offset, per_page: 10)
+      expect(page.items).to eq([])
+      expect(page.total).to eq(0)
+      expect(page.has_next?).to be false
+      expect(page.has_prev?).to be false
+    end
+
+    it 'returns empty page for empty collection with cursor strategy' do
+      page = described_class.paginate([], strategy: :cursor, per_page: 10)
+      expect(page.items).to eq([])
+      expect(page.total).to eq(0)
+    end
+  end
+
+  describe 'single item collection' do
+    it 'paginates a single item' do
+      page = described_class.paginate([42], strategy: :offset, per_page: 10)
+      expect(page.items).to eq([42])
+      expect(page.total).to eq(1)
+      expect(page.has_next?).to be false
+      expect(page.has_prev?).to be false
+    end
+  end
+
   describe Philiprehberger::Pagination::Page do
     it 'stores items and metadata' do
       page = described_class.new(items: [1, 2, 3], total: 10, next_cursor: 'abc', prev_cursor: 'def')
@@ -140,6 +214,21 @@ RSpec.describe Philiprehberger::Pagination do
     it 'returns empty links when no cursors' do
       page = described_class.new(items: [1], total: 1)
       expect(page.links).to eq({})
+    end
+
+    it 'returns only next link on first page' do
+      page = described_class.new(items: [1], total: 10, next_cursor: '2')
+      expect(page.links).to eq({ next: '2' })
+    end
+
+    it 'returns only prev link on last page' do
+      page = described_class.new(items: [1], total: 10, prev_cursor: '4')
+      expect(page.links).to eq({ prev: '4' })
+    end
+
+    it 'defaults total to nil' do
+      page = described_class.new(items: [1])
+      expect(page.total).to be_nil
     end
   end
 end
